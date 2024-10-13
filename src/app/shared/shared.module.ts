f5ec6d89fce20f0fcb423c9d4e42cd89c48239ec
Jhonny <jhonnycgarcia@gmail.com>
2024-10-12 23:34:29 -0500

    Resolución de Tarea: Contact Page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,16 +5,19 @@ import { RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPagesComponent } from './pages/contact-pages/contact-pages.component';
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
+    ContactPagesComponent,
     HomePageComponent,
     SidebarComponent,
   ],
   exports: [
     AboutPageComponent,
+    ContactPagesComponent,
     HomePageComponent,
     SidebarComponent,
   ],
