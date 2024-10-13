ea21b6af7a475f81e7dece72d2c186464a81fc04
Jhonny <jhonnycgarcia@gmail.com>
2024-10-12 23:46:46 -0500

    Countries Routing Module

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { CountriesRoutingModule } from './countries-routing.module';
 
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ByCapitalPageComponent,
+    ByCountryPageComponent,
+    ByRegionPageComponent,
+    CountryPageComponent,
+  ],
   imports: [
     CommonModule,
     CountriesRoutingModule
