37cb3a6c99af10589735d63c1ed698b909666629
Jhonny <jhonnycgarcia@gmail.com>
2024-10-12 23:26:45 -0500

    RouterLink y RouterLinkOptions

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
-
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
     HomePageComponent,
+    SidebarComponent,
   ],
   exports: [
     AboutPageComponent,
     HomePageComponent,
+    SidebarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
